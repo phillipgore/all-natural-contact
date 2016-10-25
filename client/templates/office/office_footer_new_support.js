62149d07a5bf737bce88bdde7acdfc79a2e8743d
Phillip Gore <phillipagore@me.com>
2016-10-25 13:58:32 -0500

    remove office

--- client/templates/office/office_footer_new_support.js
+++
@@ -1,6 +0,0 @@
-Template.officeFooterNewSupport.events({
-  'click .js_new_support_submit': function(e) {
-    e.preventDefault();
-    $('.js_new_support_form').submit();
-  }
-});
--- client/templates/office/office_footer_update_support.js
+++
@@ -1,6 +0,0 @@
-Template.officeFooterUpdateSupport.events({
-  'click .js_update_support': function(e) {
-    e.preventDefault();
-    $('.js_update_support_form').submit();
-  }
-});
